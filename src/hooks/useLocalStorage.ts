a0254b589b4633d897c1ca26ab84903f9e039581
NyAndoMayah <hei.mayah.3@gmail.com>
2023-06-15 11:57:27 +0300

    feat(STD21039): storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,11 @@
 const useLocalStorage = () => {
+  const addItem = (key: string, value: string) => {
+    window.localStorage.setItem(key, value);
+  };
+  const getItem = (key: string) => {
+    return window.localStorage.getItem(key);
+  };
+  return { addItem, getItem };
+};
 
-}
-
-export {
-  useLocalStorage
-}
+export { useLocalStorage };
