3f06600baa5a0be4850c5786f409a7b5f0ea91f5
NyAndoMayah <hei.mayah.3@gmail.com>
2023-06-15 11:56:43 +0300

    feat(STD21039): index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 /**
   Calculates the time difference between the server time and client time.
@@ -7,14 +8,25 @@ import { useRouter } from "next/router";
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
-
+const calculateTimeDifference = (server: Date, client: Date) => {
+  console.log(server, client);
+  console.log(server.getHours(), client.getHours());
+  return Math.abs(server?.getHours() - client.getHours());
+};
 
 export default function Home() {
   const router = useRouter();
   const moveToTaskManager = () => {
-    router.push("/tasks");
-  }
+    router.push('/tasks');
+  };
+  const [clientTime, setClientTime] = useState<Date>(new Date());
+  const [serverTime, setServerTime] = useState<Date>(
+    new Date(clientTime.getTime() + clientTime.getTimezoneOffset() * 60 * 1000)
+  );
+  const [diffTime, setDiffTime] = useState<number | null>(null);
+  useEffect(() => {
+    setDiffTime(calculateTimeDifference(serverTime, clientTime));
+  }, [clientTime, serverTime]);
   return (
     <>
       <Head>
@@ -28,14 +40,22 @@ export default function Home() {
         <div>
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
-            Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Server time:{' '}
+            <span className="serverTime">
+              {serverTime?.toLocaleString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </span>
           </p>
 
           {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Time diff:{' '}
+            <span className="serverTime">{`${diffTime?.toString()} Hours`}</span>
           </p>
         </div>
 
